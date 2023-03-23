b4b675981c2fc904acb08831b66b30a86f516994
ecmel <ecmel.ercan@gmail.com>
2023-03-23 13:18:55 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Prettier Plugin for EJS
+
+This plugin formats [EJS](https://ejs.co/) markup as comments in `html` files.
+
+## Installation
+
+```sh
+npm install --save-dev prettier-plugin-ejs
+```
+
+## Warning
+
+The greater then symbol `>` should not be used inside `<% %>`tags.
---
+++ index.ts
@@ -0,0 +1,25 @@
+import { Parser, ParserOptions } from "prettier";
+import { parsers as htmlParsers } from "prettier/parser-html";
+
+const regex = /<textarea.*?<\/textarea|"[^"]*"|(<%(.*?)%>)/gs;
+
+function parse(
+  text: string,
+  parsers: { [parserName: string]: Parser },
+  options: ParserOptions
+) {
+  return htmlParsers.html.parse(
+    text.replace(regex, (m, t, c) => (!t ? m : `<?${c}?>`)),
+    parsers,
+    options
+  );
+}
+
+export default {
+  parsers: {
+    html: {
+      ...htmlParsers.html,
+      parse,
+    },
+  },
+};
