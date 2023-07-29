a2c1a5b2c00abac0d9c33149a088af5ded707d7b
Dr. Vortex <prevett.james@gmail.com>
2023-07-29 11:46:00 -0500

    Add .ejs file support

--- index.ts
+++ index.ts
@@ -4,22 +4,29 @@ import { parsers as htmlParsers } from "prettier/parser-html";
 const regex = /"[^"]*"|<textarea.*?<\/textarea|(<%([^>]*?)%>)/gs;
 
 function parse(
-  text: string,
-  parsers: { [parserName: string]: Parser },
-  options: ParserOptions
+	text: string,
+	parsers: { [parserName: string]: Parser },
+	options: ParserOptions
 ) {
-  return htmlParsers.html.parse(
-    text.replace(regex, (m, t, c) => (!t ? m : `<?${c}?>`)),
-    parsers,
-    options
-  );
+	return htmlParsers.html.parse(
+		text.replace(regex, (m, t, c) => (!t ? m : `<?${c}?>`)),
+		parsers,
+		options
+	);
 }
 
 export default {
-  parsers: {
-    html: {
-      ...htmlParsers.html,
-      parse,
-    },
-  },
+	languages: [
+		{
+			name: "EJS",
+			parsers: "html",
+			extensions: [".ejs"]
+		}
+	],
+	parsers: {
+		html: {
+			...htmlParsers.html,
+			parse,
+		},
+	},
 };
