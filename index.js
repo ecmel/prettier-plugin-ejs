55b8664a1a4bd63197ac1451fc9e198c3a86d13a
ecmel <ecmel.ercan@gmail.com>
2023-09-13 16:39:59 +0300

    release

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { parsers } = require("prettier/parser-html");
 
 const regex =
-  /"(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*'|<title\s*>.*?<\/title|<textarea\s*>.*?<\/textarea|(<%([^>]*?)%>)/gs;
+  /"(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*'|<title.*?>.*?<\/title>|<textarea.*?>.*?<\/textarea>|(<%([^>]*?)%>)/gs;
 
 const replacer = (m, t, c) => (!t ? m : `<!${c}!>`);
 
