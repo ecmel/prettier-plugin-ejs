352d382908a6176e97f7ffdd451f22844e59bff2
ecmel <ecmel.ercan@gmail.com>
2024-01-13 21:45:25 +0300

    fixed issue 8

--- index.js
+++ index.js
@@ -6,8 +6,9 @@
 const { parsers } = require("prettier/parser-html");
 
 function parse(text, options, legacy) {
-  const find = /(?:<(textarea|title|script).*?<\/\1|(["']).*?\2|<%([^>]*)%>)/gs;
-  text = text.replace(find, (match, p1, p2, p3) => (p3 ? `<!${p3}!>` : match));
+  const find =
+    /(?:<(textarea|title|script).*?<\/\1|<[a-zA-Z!\s\\].*?(?<!%)>|<%([^>]*)%>)/gs;
+  text = text.replace(find, (match, p1, p2) => (p2 ? `<!${p2}!>` : match));
   return parsers.html.parse(text, options, legacy);
 }
 
