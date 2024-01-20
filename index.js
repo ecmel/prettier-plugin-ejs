026df7583d39010a90cfb58bd0065ed33c08bca9
ecmel <ecmel.ercan@gmail.com>
2024-01-20 17:03:34 +0300

    fix regex

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1986-2023 Ecmel Ercan (https://ecmel.dev/)
+ * Copyright (c) 1986-2024 Ecmel Ercan (https://ecmel.dev/)
  * Licensed under the MIT License
  */
 
@@ -7,7 +7,7 @@ const { parsers } = require("prettier/parser-html");
 
 function parse(text, options, legacy) {
   const find =
-    /(?:<(textarea|title|script).*?<\/\1|<\s*[a-zA-Z!].*?[^%/]>|<%([^>]*)%>)/gs;
+    /(?:<(textarea|title|script).*?<\/\1|<\s*[a-zA-Z!](?:".*?"|.*?)*?[^%]>|<%([^>]*)%>)/gs;
   text = text.replace(find, (match, p1, p2) => (p2 ? `<!${p2}!>` : match));
   return parsers.html.parse(text, options, legacy);
 }
